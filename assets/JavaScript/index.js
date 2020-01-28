4b577e3881e3efa1ae16f4e7d5a1bae67833e25c
Gregory Rutherford <gregorford@gmail.com>
2020-01-27 18:14:20 -0600

    styling mostly completed, mobile responsive

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -1,12 +1,12 @@
 const tourData = [
-  { location: "Chicago, IL", date: "2/19/19", venue: "The Empty Bottle" },
-  { location: "Colombus, OH", date: "2/20/19", venue: "Ace of Cups" },
-  { location: "Lancaster, PA", date: "2/21/19", venue: "ABAG" },
-  { location: "Jersey City, NJ", date: "2/22/19", venue: "White Eagle Hall" },
-  { location: "Philadelphia, PA", date: "2/23/19", venue: "Boot & Saddle" },
-  { location: "Albany, CA", date: "2/27/19", venue: "Ivy Room" },
-  { location: "Los Angeles, CA", date: "2/28/19", venue: "Bootleg Theater" },
-  { location: "Joshua Tree, CA", date: "2/29/19", venue: "Pappy and Harriet's" }
+  { location: "Chicago, IL", date: "2/20/20", venue: "The Empty Bottle" },
+  { location: "Colombus, OH", date: "2/20/20", venue: "Ace of Cups" },
+  { location: "Lancaster, PA", date: "2/21/20", venue: "ABAG" },
+  { location: "Jersey City, NJ", date: "2/22/20", venue: "White Eagle Hall" },
+  { location: "Philadelphia, PA", date: "2/23/20", venue: "Boot & Saddle" },
+  { location: "Albany, CA", date: "2/27/20", venue: "Ivy Room" },
+  { location: "Los Angeles, CA", date: "2/28/20", venue: "Bootleg Theater" },
+  { location: "Joshua Tree, CA", date: "2/29/20", venue: "Pappy and Harriet's" }
 ];
 
 
@@ -20,7 +20,7 @@ function displayDates(){
         const venue = show.venue;
         return `
         <div class="showInfo">
-            <li class="title">${location}</li>
+            <li class="location">${location}</li>
             <li class="date">${showDate}</li>
             <li class="venue">${venue}</li>
         </div>
