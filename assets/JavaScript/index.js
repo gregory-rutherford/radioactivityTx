ca99f6dc4fce9e018aaa868b11d02d5c02956c9f
Gregory Rutherford <46570985+gregory-rutherford@users.noreply.github.com>
2020-04-09 17:44:45 -0500

    took tour dates off for now, placed in canceledDates array

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -1,4 +1,19 @@
 const tourData = [
+  
+];
+
+const oldTourDates = [
+  { location: "Chicago, IL", date: "2/19/20", venue: "The Empty Bottle" },
+  { location: "Colombus, OH", date: "2/20/20", venue: "Ace of Cups" },
+  { location: "Lancaster, PA", date: "2/21/20", venue: "ABAG" },
+  { location: "Jersey City, NJ", date: "2/22/20", venue: "White Eagle Hall" },
+  { location: "Philadelphia, PA", date: "2/23/20", venue: "Boot & Saddle" },
+  { location: "Albany, CA", date: "2/27/20", venue: "Ivy Room" },
+  { location: "Los Angeles, CA", date: "2/28/20", venue: "Bootleg Theater" },
+  { location: "Joshua Tree, CA", date: "2/29/20", venue: "Pappy and Harriet's" },
+];
+
+const cancelledDates = [
   { location: "San Antonio, TX", date: "3/26/20", venue: "Paper Tiger" },
   { location: "Austin, TX", date: "3/27/20", venue: "Hotel Vegas" },
   { location: "Denton, TX", date: "3/28/20", venue: "Dan's Silverleaf" },
@@ -13,17 +28,6 @@ const tourData = [
   { location: "Sheffield, UK", date: "5/23/20", venue: "Delicious Clam" },
   { location: "London, UK", date: "5/24/20", venue: "The Lexington" },
   { location: "Madison, WI", date: "6/12/20", venue: "High Noon Saloon" },
-];
-
-const oldTourDates = [
-  { location: "Chicago, IL", date: "2/19/20", venue: "The Empty Bottle" },
-  { location: "Colombus, OH", date: "2/20/20", venue: "Ace of Cups" },
-  { location: "Lancaster, PA", date: "2/21/20", venue: "ABAG" },
-  { location: "Jersey City, NJ", date: "2/22/20", venue: "White Eagle Hall" },
-  { location: "Philadelphia, PA", date: "2/23/20", venue: "Boot & Saddle" },
-  { location: "Albany, CA", date: "2/27/20", venue: "Ivy Room" },
-  { location: "Los Angeles, CA", date: "2/28/20", venue: "Bootleg Theater" },
-  { location: "Joshua Tree, CA", date: "2/29/20", venue: "Pappy and Harriet's" }
 ];
 
 const datesBox = document.querySelector(".datesBox");
